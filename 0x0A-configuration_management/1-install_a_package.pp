877e0a211402d686e8a346d38b21320120a22834
ClintonCMOs <orengecmo@gmail.com>
2024-02-23 12:06:51 -0800

    install_flask.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# Install Flask from pip3
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+class flask_install {
+  package { 'Flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+  }
 }
+
+include flask_install
