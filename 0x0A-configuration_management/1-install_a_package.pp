c20e8e6697be79e34eb13bc8b06411ffdc3077e6
ClintonCMOs <orengecmo@gmail.com>
2024-02-23 12:02:03 -0800

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-#Install the package
-package {'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem',
+# Install Flask from pip3
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
