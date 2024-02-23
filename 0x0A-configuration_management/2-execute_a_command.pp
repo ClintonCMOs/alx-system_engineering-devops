9ef619c10fbff78b57709de93ae68055685e5851
ClintonCMOs <orengecmo@gmail.com>
2024-02-23 15:32:59 -0800

    updated file

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,3 +1,8 @@
-#kills killmeow
-exec {'pkill killmenow':
+# Kills a process name killmenow
+
+exec { 'pkill killmenow':
+  path     => '/usr/bin',
+  command  => 'pkill killmenow',
+  provider => shell,
+  returns  => [0, 1]
 }
