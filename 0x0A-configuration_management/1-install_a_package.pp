bcaa98a8f808ec9f0ebd5fd0dee7f1ff87f31c25
ClintonCMOs <orengecmo@gmail.com>
2024-02-23 16:03:25 -0800

    updated file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#install a flask from pip3
 exec { 'install flask':
-  command =>  'pip3 install flask==2.1.0',
-  path    =>  '/usr/bin/',
-  unless  =>  'pip3 list | grep flask',
+  command => 'pip3 install "Flask==2.1.0" "Werkzeug==2.1.1"',
+  path    => '/usr/bin/',
+  unless  => 'pip3 list | grep -q "Flask==2.1.0"',
 }
