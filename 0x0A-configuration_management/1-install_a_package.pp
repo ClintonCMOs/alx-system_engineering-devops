eab25aaf7c3baae03875925a28422054954c238e
ClintonCMOs <orengecmo@gmail.com>
2024-02-23 16:18:18 -0800

    new file

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-# installs flask package
+# install puppet-lint -v 2.5.0
 
-package {'flask':
-  ensure   => '2.1.0',
-  name     => 'flask',
-  provider => 'pip3'
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.1.0',
 }
