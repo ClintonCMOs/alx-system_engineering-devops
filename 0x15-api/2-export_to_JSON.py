99f05bd83a62c29f444c1adb93fc0783bee94e7d
ClintonCMOs <orengecmo@gmail.com>
2024-03-25 17:00:20 +0300

    2-export_to_JSON.py

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+The script will fetch data from a jsonplaceholder API
+about an employee's TODO list progress
+It will accept an employee ID as an integer parameter
+and print the information in the specified format
+
+After that, it will export data in the JSON format
+"""
+
+import json
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    dictionary = {employeeId: []}
+    for task in tasks:
+        dictionary[employeeId].append({
+            "task": task.get('title'),
+            "completed": task.get('completed'),
+            "username": username
+        })
+    with open('{}.json'.format(employeeId), 'w') as f:
+        json.dump(dictionary, f)
