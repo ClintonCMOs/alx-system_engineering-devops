de6b3b0717436d1aca762b98ce70d2038f2a816e
ClintonCMOs <orengecmo@gmail.com>
2024-03-25 15:50:52 +0300

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+The script will fetch data from a fictional API 
+about an employee's TODO list progress
+It will accept an employee ID as an integer parameter 
+and print the information in the specified format
+"""
+
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get("name")
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    finished = 0
+    finished_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            finished_tasks.append(task)
+            finished += 1
+
+    print("Employee {} is done with tasks({}/{}):"
+          .format(employeeName, finished, len(tasks)))
+
+    for task in finished_tasks:
+        print("\t {}".format(task.get('title')))
---
+++ 0x15-api/README.md
@@ -0,0 +1,3 @@
+0x15-api
+Gathering Data from an Application Programming Interface (API)
+Use REST API
