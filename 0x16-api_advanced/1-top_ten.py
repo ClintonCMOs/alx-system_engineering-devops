aa8bf95a41ff203c054071d92334fb7def45e8df
ClintonCMOs <orengecmo@gmail.com>
2024-04-12 05:43:50 +0300

    updated 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,33 +1,24 @@
 #!/usr/bin/python3
-"""
-Function queries the Reddit API and prints the first
-10 hot posts listed for a given subreddit
-"""
+'''
+    this module contains the function top_ten
+'''
 import requests
+from sys import argv
 
 
 def top_ten(subreddit):
-    """Prints the first 10 hot posts for a subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    '''
+        Returns top ten posts for a given subreddit
+    '''
+    user = {'User-Agent': 'Lizzie'}
+    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
+                       .format(subreddit), headers=user).json()
+    try:
+        for post in url.get('data').get('children'):
+            print(post.get('data').get('title'))
+    except Exception:
+        print(None)
 
-    # Define a custom User-Agent to avoid Reddit API issues
-    headers = {'User-Agent': 'Custom User-Agent'}
 
-    # Send a GET request to the Reddit API
-    response = requests.get(url, headers=headers)
-
-    # Check is the request was successful
-    if response.status_code == 200:
-        # Parse the JSOn response
-        data = response.json()
-
-        # Check if the subreddit exists and contains titles
-        if 'data' in data and 'children' in data['data']:
-            # Loop through the first 10 posts and print their titles
-            for post in data['data']['children']:
-                title = post['data']['title']
-                print(title)
-        else:
-            print("None")
-    else:
-        print("None")
+if __name__ == "__main__":
+    top_ten(argv[1])
