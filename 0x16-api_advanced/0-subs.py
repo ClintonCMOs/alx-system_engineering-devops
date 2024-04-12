c84d3bb2d791d0be6a00efec909cfc89c48ded19
ClintonCMOs <orengecmo@gmail.com>
2024-04-12 05:50:28 +0300

    updated 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,27 @@
 #!/usr/bin/python3
-# get subsscribers
-from requests import get
-from sys import argv
+"""
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
+"""
+import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """subs"""
-    head = {'User-Agent': 'Dan Kazam'}
-    count = get('https://www.reddit.com/r/{}/about.json'.format(
-        subreddit), headers=head).json()
-    try:
-        return count.get('data').get('subscribers')
-    except:
-        return 0
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
 
-if __name__ == "__main__":
-    number_of_subscribers(argv[1])
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
+        return 0
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
