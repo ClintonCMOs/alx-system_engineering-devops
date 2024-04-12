4c1d6ae050430feab0c1328d5baefff71870c25d
ClintonCMOs <orengecmo@gmail.com>
2024-04-12 05:58:44 +0300

    updated 2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,20 +8,20 @@ import sys
 
 
 def number_of_subscribers(subreddit):
-    """ Queries to Reddit API """
-    u_agent = 'Mozilla/5.0'
+    """
+    Queries the Reddit API to get the number of subscribers
+    If the subreddit is not valid, returns 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    headers = {
-        'User-Agent': u_agent
-    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    res = requests.get(url, headers=headers, allow_redirects=False)
-    if res.status_code != 200:
+    if response.status_code != 200:
+        return 0  # Return 0 for invalid subreddits or other errors
+
+    try:
+        data = response.json()
+        return data['data']['subscribers']
+    except (KeyError, TypeError):
         return 0
-    dic = res.json()
-    if 'data' not in dic:
-        return 0
-    if 'subscribers' not in dic.get('data'):
-        return 0
-    return res.json()['data']['subscribers']
